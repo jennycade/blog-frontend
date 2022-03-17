62ece7d943b666e45c40c9cf9af1f68513e85ed9
Jenny Zonka <jennycade@gmail.com>
2022-03-16 18:08:31 -0700

    Started making update comment functionality work.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -17,7 +17,29 @@ const Comment = (props) => {
   const [text, setText] = useState(comment.text);
 
   // functions
+  const handleEditClick = (e) => {
+    e.currentTarget.blur();
+    setEditing(true);
+  }
   const handleUpdateText = (e) => {
+    setText(e.target.value);
+  }
+  const handleSubmitUpdate = async (e) => {
+    e.currentTarget.blur();
+    setEditing(false);
+    await updateComment(text);
+    // FIX: postId not going through to API
+
+  }
+  const handleCancelClick = (e) => {
+    e.currentTarget.blur();
+    // reset text
+    setText(comment.text);
+    // reset editing
+    setEditing(false);
+  }
+  const handleDeleteClick = (e) => {
+    e.currentTarget.blur();
     // TODO
   }
 
@@ -29,15 +51,34 @@ const Comment = (props) => {
   return (
     <article key={comment._id}>
       <Byline article={comment} />
-      <p>
-        {comment.text}
-      </p>
-      { userId === comment.author._id &&
-        <div className='buttonSet hiddenUntilHovering'>
-          <button onClick={setEditing(true)}>Edit Comment</button>
-          <button>Delete Comment</button>
-        </div>
+      { editing ?
+        <>
+          <textarea value={text}
+            onChange={handleUpdateText}
+          />
+          <button onClick={handleSubmitUpdate}>Update</button>
+          <button onClick={handleCancelClick}>Cancel</button>
+        </>
+        :
+        <>
+          <p>
+            {comment.text}
+          </p>
+          { userId === comment.author._id &&
+            <div className='buttonSet hiddenUntilHovering'>
+              <button
+                onClick={handleEditClick}
+                className='outline'
+              >
+                Edit Comment
+              </button>
+              <button onClick={handleDeleteClick}>Delete Comment</button>
+            </div>
+          }
+        </>
       }
+      
+      
     </article>
   );
 }
